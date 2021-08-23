4bdd0e1a1eebe2282f6dd2feb9ecc9a64b102648
clk <clk528@qq.com>
2021-08-23 13:37:49 +0800

    upadte

--- src/Traits/JiaoweiTrait.php
+++ src/Traits/JiaoweiTrait.php
@@ -23,7 +23,7 @@ trait JiaoweiTrait
      */
     private function decrAccess($netid)
     {
-        $this->accessService->decrAllAccessTimeByNetId($netid, '2020-09-01 12:00:00', '系统禁用:未进行入学申报或未通过安全培训');
+        $this->accessService->decrAllAccessTimeByNetId($netid, '2020-09-01 12:00:00', '系统禁用:未进行入学申报');
     }
 
     /**
@@ -32,7 +32,7 @@ trait JiaoweiTrait
      */
     private function incrAccess($netid)
     {
-        $this->accessService->incrAllAccessTimeByNetId($netid, '系统启用:入学申报完成以及通过安全培训');
+        $this->accessService->incrAllAccessTimeByNetId($netid, '系统启用:入学申报完成');
     }
 
     /**
@@ -44,7 +44,7 @@ trait JiaoweiTrait
         $list = [
             $netId
         ];
-        $this->accessService->editBadge($list, 'enable', '系统禁用:入学申报完成以及通过安全培训');
+        $this->accessService->editBadge($list, 'enable', '系统禁用:入学申报完成');
     }
 
     /**
@@ -56,7 +56,7 @@ trait JiaoweiTrait
         $list = [
             $netId
         ];
-        $this->accessService->editBadge($list, 'disable', '系统禁用:未进行入学申报或未通过安全培训');
+        $this->accessService->editBadge($list, 'disable', '系统禁用:未进行入学申报');
     }
 
     /**
@@ -99,41 +99,68 @@ trait JiaoweiTrait
             return false;
         }
 
-        if ($isEnableMode) { // 如果是调用模式
-            if ($this->endTime2021($user['endTime'] ?? null, $netId)) {// 返回为true 为不满足条件
-                return false;
+        if ($isEnableMode) { // 如果是解封模式
+            if (!$this->endTime2021($user['endTime'] ?? null, $netId)) {// 返回为false代表未完成入学申报的流程
+                return false; // 未完成入学申报
             }
         }
 
-        // 2: endTime 小于2021-08-20
+        // 2: endTime 小于2021-02-01
         if (!$isEnableMode) { //如果是禁用模式
-            if ($this->endTime2021($user['endTime'] ?? null, $netId)) {// 返回为true 代表
-                $this->info("第{$this->index}个人，NetId:{$netId} endTime为：" . ($user['endTime'] ?? null) . "，小于2021-08-20 需要封禁");
-                return false;
+            if ($this->endTime2022($user['endTime'] ?? null, $netId)) {// 返回为true符合封禁条件
+                $this->info("第{$this->index}个人，NetId:{$netId} endTime为：" . ($user['endTime'] ?? null) . "，小于2021-02-01 需要封禁");
+                return false; // 未完成入学申报
             }
         }
-//        if ($this->workFlowExpireTime($user['returnTime'] ?? null, $netId)) { // 返回为true 为不满足条件（endTime 小于2021-08-20)
-//            return false;
-//        }
 
         // 3：三个码全部不为false
         return !$user['health'] && !$user['tourCode'] && !$user['healthCode'];
     }
 
+    /**
+     * 判断是否符合解封条件
+     * @param $endTime
+     * @param $netId
+     * @return bool
+     */
     private function endTime2021($endTime, $netId)
     {
-        if (empty($endTime)) {
-            return true;
+        if (empty($endTime)) { // 没有做过申报
+            return false;
         }
 
         $now = strtotime("2021-08-20 00:00:00");
         $reportTime = strtotime($endTime);
 
         if ($reportTime >= $now) { // 报告时间大于 2021-08-20
-            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，可以解封");
+            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，符合解封条件");
+            return true;
+        } else {
+            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，不符合解封条件");
+            return false;
+        }
+    }
+
+    /**
+     * 判断是否需要封禁
+     * @param $endTime
+     * @param $netId
+     * @return bool
+     */
+    private function endTime2022($endTime, $netId)
+    {
+        if (empty($endTime)) {
+            return true;
+        }
+
+        $now = strtotime("2021-02-01 00:00:00");
+        $reportTime = strtotime($endTime);
+
+        if ($reportTime >= $now) { // 报告时间大于 2021-02-01
+            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，报告时间大于2021-02-01 不需要封禁");
             return false;
         } else {
-            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，不可以解封");
+            $this->info("第{$this->index}个人，NetId:{$netId}报告日期为：{$endTime}，报告时间小于2021-02-01 需要封禁");
             return true;
         }
     }
